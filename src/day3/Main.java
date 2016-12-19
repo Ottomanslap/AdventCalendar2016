0d968d43acd106e0d6097e693f4f4eddd06f9e65
baldr <baldr@192.168.135.190>
2016-12-19 23:20:10 +0000

    day 3

---
+++ src/day3/Main.java
@@ -0,0 +1,40 @@
+//package day3;
+//
+//import java.io.IOException;
+//import java.util.ArrayList;
+//import java.util.NoSuchElementException;
+//import java.util.Scanner;
+//
+//class Main {
+//	
+//	private static Scanner input = new Scanner(System.getProperty("java.class.path") + "//..//Files//day2");
+//	
+//	static ArrayList<String> lines  = new ArrayList<String>();
+//	static ArrayList<Integer> nums1 = new ArrayList<Integer>();
+//	static ArrayList<Integer> nums2 = new ArrayList<Integer>();
+//	static ArrayList<Integer> nums3 = new ArrayList<Integer>();
+//
+//	public static void main(String[] areas) throws IOException {
+//		
+//		while (true){
+//			try{
+//				lines.add(input.nextLine());
+//			}catch (NoSuchElementException e) {
+//				break;
+//			}
+//		}
+//		
+//		for (String line : lines){
+//			ArrayList<Integer> y = new ArrayList<Integer>();
+//			ArrayList<String>  x = new ArrayList<String> ();
+//			x.addAll(line.split(" "));
+//			for(String num : x){
+//				
+//			}
+//			nums1.add(y[0]);
+//			nums2.add(y[1]);
+//			nums2.add(y[2]);
+//		}
+//	}
+//
+//}
